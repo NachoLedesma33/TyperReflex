7daba378b69f43684ed6dcfcd61d98458c50b033
Nacho Ledesma <nacholedesma33@gmail.com>
2026-08-03 14:20:56 -0300

    feat: add theme provider, toggle and app providers

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,20 @@
-import { StrictMode } from "react"
-import { createRoot } from "react-dom/client"
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { registerSW } from "virtual:pwa-register";
 
-import "./index.css"
-import App from "./App.tsx"
-import { ThemeProvider } from "@/components/theme-provider.tsx"
+import "./index.css";
+import App from "./App.tsx";
+import { ThemeProvider } from "@/components/theme-provider.tsx";
+import { SettingsProvider } from "@/lib/settings.tsx";
+
+registerSW({ immediate: true });
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider defaultTheme="light" storageKey="typerreflex-theme">
-      <App />
+      <SettingsProvider>
+        <App />
+      </SettingsProvider>
     </ThemeProvider>
   </StrictMode>
-)
+);
